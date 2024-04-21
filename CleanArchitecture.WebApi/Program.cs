c1c253f15593efe2c45c05567bbaf04da7995778
Berken Yünlü <berkenyunlu@gmail.com>
2024-04-21 16:29:17 +0300

    db configuration and entites

--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,8 +1,14 @@
+using CleanArchitecture.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+builder.Services.AddControllers().AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ src/External/CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using CleanArchitecture.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistance.Context;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var entries = ChangeTracker.Entries<Entity>();
+        foreach (var entry in entries)
+        {
+            if(entry.State==EntityState.Added)
+                entry.Property(x=>x.CreatedDate).CurrentValue=DateTime.Now;
+           if(entry.State==EntityState.Modified)
+               entry.Property(x=>x.UpdatedDate).CurrentValue=DateTime.Now;
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
